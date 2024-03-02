8799943d17909372a131165b6bec1db4f0e16e4e
TobiasOnandia <tobiasonandia0@gmail.com>
2024-03-02 17:56:58 -0300

    actualizar

--- src/services/useUsers.tsx
+++ src/services/useUsers.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import { User } from "../types.d";
+import { API } from "../API";
 
 export const useUsers = () => {
   const [users, setUsers] = useState<User[]>([]);
   const ref = useRef<User[]>([]);
 
   useEffect(() => {
-    fetch("https://randomuser.me/api/?results=10 ")
+    fetch(API)
       .then(async (json) => await json.json())
       .then((res) => {
         ref.current = res.results;
